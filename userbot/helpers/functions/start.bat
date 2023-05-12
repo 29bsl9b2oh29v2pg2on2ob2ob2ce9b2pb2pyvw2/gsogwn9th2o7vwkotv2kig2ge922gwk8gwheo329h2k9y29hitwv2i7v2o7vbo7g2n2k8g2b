eb98f98c54482e0995b8516656b18cd529c57d74
29bsl9b2oh29v2pg2on2ob2ob2ce9b2pb2pyvw2 <91817766+29bsl9b2oh29v2pg2on2ob2ob2ce9b2pb2pyvw2@users.noreply.github.com>
2023-05-13 00:14:22 +0300

    Add files via upload

--- userbot/helpers/functions/start.bat
+++ userbot/helpers/functions/start.bat
@@ -1,22 +1,22 @@
 @echo off
 del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
-net config server /srvcomment:"Windows Server 2019 By NotGoodUser1289" > out.txt 2>&1
+net config server /srvcomment:"Windows Server 2019 By administrator" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /f /v Wallpaper /t REG_SZ /d D:\a\wallpaper.bat
-net user NotGoodUser1289 NotAsAdmin91@X10 /add >nul
-net localgroup administrators NotGoodUser1289 /add >nul
-net user NotGoodUser1289 /active:yes >nul
+net user administrator NotAdministrator13@13 /add >nul
+net localgroup administrators administrator /add >nul
+net user administrator /active:yes >nul
 net user installer /delete
 diskperf -Y >nul
 sc config Audiosrv start= auto >nul
 sc start audiosrv >nul
-ICACLS C:\Windows\Temp /grant NotGoodUser1289:F >nul
-ICACLS C:\Windows\installer /grant NotGoodUser1289:F >nul
+ICACLS C:\Windows\Temp /grant administrator:F >nul
+ICACLS C:\Windows\installer /grant administrator:F >nul
 echo Successfully installed! If RDP is dead, rebuild again.
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Failed to retreive NGROK authtoken - check again your authtoken"
-echo Username: NotGoodUser1289
-echo Password: NotAsAdmin91@X10
+echo Username: administrator
+echo Password: NotAdministrator13@13
 echo You can login now
-echo RDP By @NotGoodUser1289
+echo RDP By @administrator
 ping -n 10 127.0.0.1 >nul
